49a1f341451586015ff63ae2f41a51faae6de325
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-28 15:31:38 -0800

    correcting linter errors

--- scroll-animation.js
+++ scroll-animation.js
@@ -2,7 +2,7 @@ const boxes = document.querySelectorAll('.box')
 window.addEventListener('scroll', checkBoxes)
 
 checkBoxes()
-function checkBoxes() {
+function checkBoxes () {
   const triggerBottom = window.innerHeight / 5 * 4
 
   boxes.forEach(box => {
@@ -10,7 +10,6 @@ function checkBoxes() {
 
     if (boxTop < triggerBottom) {
       box.classList.add('show')
-
     } else {
       box.classList.remove('show')
     }
